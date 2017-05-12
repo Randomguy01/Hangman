7887526a70c4c573ebab0b2bf579c5972883a5da
Randomguy01 <ianhwhite2001@gmail.com>
2017-05-12 12:14:30 -0400

    Init

---
+++ src/Hangman.java
@@ -0,0 +1,12 @@
+/**
+ * Ian White
+ * Program:
+ * ${Date}
+ * JDK 1.8
+ */
+public class Hangman {
+    private static String mPhrase;
+    private static int mGuesses;
+    private static int mLives;
+    private static char[] mGuessedLetters;
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Ian White
+ * Program:
+ * ${Date}
+ * JDK 1.8
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
