af78bea622e04133eda9c296ea75af3a89ae22d4
Randomguy01 <ianhwhite2001@gmail.com>
2017-05-12 12:28:11 -0400

    Added Skeleton Class for Hangman

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Ian White
  * Program:
@@ -6,8 +8,52 @@
  */
 public class Hangman {
     private static String mPhrase;
-    private static int mGuesses;
+    private static int mGuesses = 6;
     private static int mLives;
-    private static char[] mGuessedLetters;
-    
+    private static ArrayList<Character> mGuessedLetters = new ArrayList<>();
+    private static char[] mPhraseArray;
+
+    public Hangman(String phrase) {
+        mPhrase = phrase;
+    }
+
+    public static String getPhrase() {
+        return mPhrase;
+    }
+
+    public static void setPhrase(String phrase) {
+        Hangman.mPhrase = phrase;
+    }
+
+    public static int getGuesses() {
+        return mGuesses;
+    }
+
+    public static int getLives() {
+        return mLives;
+    }
+
+    public static ArrayList<Character> getGuessedLetters() {
+        return mGuessedLetters;
+    }
+
+    public static boolean guess(char letter){
+        mGuessedLetters.add(letter);
+
+        for (int i = 0; i < mPhraseArray.length; i++) {
+            if (mPhraseArray[i] == letter && !isGuessed(letter)){
+
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isGuessed(char letter){
+        for (char guessedLetter : mGuessedLetters){
+            if (guessedLetter == letter)
+                return true;
+        }
+        return false;
+    }
 }
