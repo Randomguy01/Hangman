9b8e3025eb2538db896c9e1bc29a2e2359e46d3d
Randomguy01 <ianhwhite2001@gmail.com>
2017-05-18 18:09:29 -0400

    Code Cleanup

--- src/Hangman.java
+++ src/Hangman.java
@@ -6,24 +6,28 @@ import java.util.ArrayList;
  * ${Date}
  * JDK 1.8
  */
-public class Hangman {
+class Hangman {
+    @SuppressWarnings("FieldCanBeLocal")
     private final String CORRECT_GUESS = "Your guess is correct";
+    @SuppressWarnings("FieldCanBeLocal")
     private final String INCORRECT_GUESS = "Your guess is incorrect";
+    @SuppressWarnings("FieldCanBeLocal")
     private final String DUPLICATE_GUESS = "You have already guessed ";
     private final String NO_LIVES_MESSAGE = "Sorry, you are out of lives. The correct phrase was: ";
     private final String WIN_MESSAGE = "CORRECT! The phrase was ";
     private String mPhrase;
     private int mGuesses = 6;
     private int mLives = 7;
-    private ArrayList<Character> mGuessedLetters = new ArrayList<>();
-    private char[] mPhraseArray;
-    private ArrayList<Character> mCorrectLetters = new ArrayList<>();
+    private final ArrayList<Character> mGuessedLetters = new ArrayList<>();
+    private final char[] mPhraseArray;
+    private final ArrayList<Character> mCorrectLetters = new ArrayList<>();
 
     public Hangman(String phrase) {
         mPhrase = phrase;
         mPhraseArray = mPhrase.toLowerCase().toCharArray();
     }
 
+    @SuppressWarnings("WeakerAccess")
     public String getPhrase() {
         return mPhrase;
     }
@@ -53,48 +57,48 @@ public class Hangman {
         return mGuessedLetters;
     }
 
-    public String guess(char letter){
+    public String guess(char letter) {
         letter = Character.toLowerCase(letter);
-        if (hasWon()){
+        if (hasWon()) {
             return WIN_MESSAGE + this.getPhrase();
-        }else if (mLives < 1){
+        } else if (mLives < 1) {
             return NO_LIVES_MESSAGE + this.getPhrase();
-        } else if (contains(mPhraseArray, letter) && !isGuessed(letter)){
+        } else if (contains(mPhraseArray, letter) && isNotGuessed(letter)) {
             mGuesses++;
             mCorrectLetters.add(letter);
             mGuessedLetters.add(letter);
             return CORRECT_GUESS;
-        }else if (!isGuessed(letter)){
+        } else if (isNotGuessed(letter)) {
             mGuesses++;
             mGuessedLetters.add(letter);
             mLives--;
             return INCORRECT_GUESS;
-        }else {
+        } else {
             return DUPLICATE_GUESS + letter;
         }
 
     }
 
-    private boolean isGuessed(char letter){
-        for (char guessedLetter : mGuessedLetters){
+    private boolean isNotGuessed(char letter) {
+        for (char guessedLetter : mGuessedLetters) {
             if (guessedLetter == letter)
-                return true;
+                return false;
         }
-        return false;
+        return true;
     }
 
     private boolean contains(char[] array, char letter) {
-        for (char position : array){
-            if (position == letter){
+        for (char position : array) {
+            if (position == letter) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean contains(ArrayList<Character> array, Character letter){
-        for (Character position : array){
-            if (position == letter){
+    private boolean contains(ArrayList<Character> array, Character letter) {
+        for (Character position : array) {
+            if (position == letter) {
                 return true;
             }
         }
@@ -102,8 +106,8 @@ public class Hangman {
     }
 
     public boolean hasWon() {
-        for (int i = 0; i < mPhrase.length(); i++){
-            if (!contains(mGuessedLetters, mPhraseArray[i])){
+        for (int i = 0; i < mPhrase.length(); i++) {
+            if (!contains(mGuessedLetters, mPhraseArray[i])) {
                 return false;
             }
         }
--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,17 @@ import java.awt.*;
  */
 //boogly boo boo hoo hoo
 public class Main extends JComponent {
+    @SuppressWarnings("FieldCanBeLocal")
     private static Hangman mHangmanGame;
-    JFrame mFrame = new JFrame();
-    Container mContainer = mFrame.getContentPane();
+    private final JFrame mFrame = new JFrame();
+    private final Container mContainer = mFrame.getContentPane();
     JPanel mParentPanel = new JPanel(new GridBagLayout());//Main Panel to hold all subpanels
     JPanel mPhrasePanel, mFigurePanel, mAlphabetPanel, mStatsPanel = new JPanel();
 
     public static void main(String[] args) {
         Main Main = new Main();
         Main.setUp();
-        boolean shouldPlay = true;
+        boolean shouldPlay;
         do {
             playGame();
             shouldPlay = false;
@@ -27,11 +28,11 @@ public class Main extends JComponent {
                     , 'y')) {
                 shouldPlay = true;
             }
-        }while (shouldPlay);
+        } while (shouldPlay);
 
     }
 
-    private static void playGame(){
+    private static void playGame() {
         mHangmanGame = new Hangman(JOptionPane.showInputDialog(null, "Enter phrase: "));
         while (mHangmanGame.getLives() > 0 && !mHangmanGame.hasWon()) {
             showMessage(mHangmanGame.guess(JOptionPane.showInputDialog(null, "Enter a guess").charAt(0))
@@ -43,13 +44,18 @@ public class Main extends JComponent {
             showMessage(mHangmanGame.getNO_LIVES_MESSAGE());
         }
     }
-    private static boolean contains(String string, char character){
+
+    private static boolean contains(String string, char character) {
         char[] stringArray = string.toLowerCase().toCharArray();
-        for (char arrayChar : stringArray){
+        for (char arrayChar : stringArray) {
             if (arrayChar == character)
                 return true;
         }
         return false;
+    }
+
+    private static void showMessage(String message) {
+        JOptionPane.showMessageDialog(null, message);
     }
 
     public void setUp() {
@@ -59,19 +65,17 @@ public class Main extends JComponent {
         mFrame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
         mFrame.setUndecorated(true);
         mFrame.setLocationRelativeTo(null);
-        mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mFrame.setVisible(true);
         mFrame.add(mContainer);
-    }
-
-    private static void showMessage(String message){
-        JOptionPane.showMessageDialog(null, message);
     }
 
     @Override
     public void paintComponent(Graphics g) {
 
     }
-    public void drawCharacterSlots(Graphics g, String mPhrase, char correctLetters[]){
+
+    public void drawCharacterSlots(Graphics g, String mPhrase, char correctLetters[]) {
+        //TODO: use for loop to draw apropriate lines
     }
 }
